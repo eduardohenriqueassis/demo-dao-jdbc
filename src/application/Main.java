3b42654a46c5165bc600a88c4db6a6a2d1951c88
Eduardo Assis <84932877+eduardohenriqueassis@users.noreply.github.com>
2024-01-28 19:46:01 -0300

    Classe Department

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,10 @@
 package application;
 
+import model.entities.Department;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
     }
 }
