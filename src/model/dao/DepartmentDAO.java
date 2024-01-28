26bb8bc8c66f079e70bc4b04ef784162579926fc
Eduardo Assis <84932877+eduardohenriqueassis@users.noreply.github.com>
2024-01-28 20:00:28 -0300

    aula 279 - DepartmentDao, SellerDao

---
+++ src/model/dao/DepartmentDAO.java
@@ -0,0 +1,16 @@
+package model.dao;
+
+import model.entities.Department;
+
+import java.util.List;
+
+public interface DepartmentDAO {
+    void insert(Department obj);
+
+    void update(Department obj);
+
+    void deleteById(Department obj);
+
+    Department findById(Integer id);
+    List<Department> findAll();
+}
